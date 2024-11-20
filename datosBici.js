0b350c38930a6d7c7cc092c2110aed21c839703a
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-11-20 18:20:05 -0300

    Cambios en el código

--- datosBici.js
+++ datosBici.js
@@ -1,24 +1,17 @@
 
 const fs = require('fs');
 
-
-
 function listaDeBicis(callback) {
-fs.readFile('bicicletas.json', 'utf8', (err, data) => {
-    
-    if (err) {
-        console.error('Error al leer el archivo JSON:', err);
+    fs.readFile('bicicletas.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo JSON:', err);
+            return;
+        }
         
-        return;
-    }
-    
-    const bicicletas = JSON.parse(data);
-
-    callback(bicicletas);
-}); 
+        const bicicletas = JSON.parse(data);
+        callback(bicicletas);
+    });
 }
-
-console.log(listaDeBicis());
 
 
 module.exports = {
