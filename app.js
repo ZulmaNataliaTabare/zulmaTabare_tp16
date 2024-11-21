f1ab5269a0cd9c2b517db0d9124c74ac41654b47
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-11-21 12:27:46 -0300

    Realicé la función buscarBici

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 
 const { listaDeBicis } = require('./datosBici');
 
-
-
-
 let dhBici = {
+    bicicletas: [],
     
-    bicicletas :  listaDeBicis((bicicletas) => {
-    console.log(bicicletas);
-}),
-    buscarBici: function (id) {
-        return this.bicicletas.filter(bici => bici.id === id )
-    }, 
-
+    inicializarBicis: function(callback) {
+        listaDeBicis((bicicletas) => {
+            this.bicicletas = bicicletas;
+            if (callback) callback();
+        });
+    },
     
-},
+    buscarBici: function(id) {
+        const biciEncontrada = this.bicicletas.filter(bici => bici.id === id);
+        return biciEncontrada ? biciEncontrada : null;
+    }
+};
 
+
+dhBici.inicializarBicis(() => {
+    const resultado = dhBici.buscarBici(10);
+    console.log(resultado);
+});
 
 
 
